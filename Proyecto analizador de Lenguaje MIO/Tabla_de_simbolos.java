ace2aa1c73bb40b0d142398ed9901a044b31efeb
JesusChay <112056157+JesusChay@users.noreply.github.com>
2023-12-10 21:52:43 -0600

    Update Tabla_de_simbolos.java

--- Proyecto analizador de Lenguaje MIO/Tabla_de_simbolos.java
+++ Proyecto analizador de Lenguaje MIO/Tabla_de_simbolos.java
@@ -1,94 +1,82 @@
 package analizador.sintactico.semantico.mio;
 
-public class Tabla_de_simbolos{
-    String Identificadores[][];
-    String LiteralesTxt[][];
-    String LiteralesNum[][];
+public class Tabla_de_simbolos {
+    String Identificadores[][] = new String[11][3];
+    String LiteralesTxt[][] = new String[11][3];
+    String LiteralesNum[][] = new String[11][3];
     String PalabrasReservadas[] = {"PROGRAMA", "FINPROG", "SI", "ENTONCES", "SINO", "FINSI", "REPITE", "VECES", "FINREP", "IMPRIME", "LEE"};
     String Operadores[] = {"+", "-", "*", "/", "<", ">", "#", "=", "=="};
 
-    public String verificarExistencia(char cadena[]){
-        String Cadena = cadena.toString();
-       for(int i = 0; i < 11 ; i++){
-           if(Cadena == PalabrasReservadas[i]){
-               return Cadena;
-           }else if(Cadena == Operadores[i]){
-               return Cadena;
-           }else if(Cadena == Identificadores[i][2]){
-               return Cadena;
-           }else if(Cadena == LiteralesTxt[i][2]){
-               return Cadena;
-           }else if(Cadena == LiteralesNum[i][2]){
-               return Cadena;
-           }
-       }
-       return Cadena = null;
-    }
-
-    public String verificarIdentificador(char Caden[]){
-        String Cadena = Caden.toString();
-        if (Caden[0] != (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | '"')) {
-            if (Cadena.length() <= 16 ) {
-                int i = 0;
-                while(Cadena != null){
-                    if(Identificadores[i][1] == null){
-                        Identificadores[i][1] = "ID"+ i;
-                        Identificadores[i][2] = Cadena;
-                        return Identificadores[i][1];
-                    }
-                    i++;
-                }
-            }else{
-
-            }   
+    public String verificarExistencia(String cadena) {
+    for (int i = 0; i < 11; i++) {
+        if ((i < PalabrasReservadas.length && cadena.equals(PalabrasReservadas[i])) ||
+            (i < Operadores.length && cadena.equals(Operadores[i])) ||
+            (i < Identificadores.length && Identificadores[i] != null && Identificadores[i].length > 2 && cadena.equals(Identificadores[i][2])) ||
+            (i < LiteralesTxt.length && LiteralesTxt[i] != null && LiteralesTxt[i].length > 2 && cadena.equals(LiteralesTxt[i][2])) ||
+            (i < LiteralesNum.length && LiteralesNum[i] != null && LiteralesNum[i].length > 2 && cadena.equals(LiteralesNum[i][2]))) {
+            return cadena;
         }
-        return Cadena = null;
     }
+    return null;
+}
 
-    public String verificarLiteralesTxt(char Caden[]){
-        String Cadena = Caden.toString();
-        if (Caden[0] == '"') {
-            if (Caden[Cadena.length()-1] == '"') {
-                int i = 0;
-                while(Cadena != null){
-                    if(LiteralesTxt[i][1] == null){
-                        LiteralesTxt[i][1] = "TX"+ i;
-                        LiteralesTxt[i][2] = Cadena;
-                        return LiteralesTxt[i][1];
-                    }
-                    i++;
-                }
-            }else{
-                    
-            }   
-        }
-        return Cadena = null;
-
-    }
-    
-    public String verificarLiteralesNum(char Caden[]){
-        String Cadena = Caden.toString();
-        if (Caden[0] == '0' && Caden[1] == ('X' | 'x') ) {   
+    public String verificarIdentificador(String cadena) {
+    if (cadena.length() > 0 && cadena.charAt(0) != '0' && cadena.charAt(0) != '1' && cadena.charAt(0) != '2' &&
+            cadena.charAt(0) != '3' && cadena.charAt(0) != '4' && cadena.charAt(0) != '5' &&
+            cadena.charAt(0) != '6' && cadena.charAt(0) != '7' && cadena.charAt(0) != '8' &&
+            cadena.charAt(0) != '9' && cadena.charAt(0) != '"') {
+        if (cadena.length() <= 16) {
             int i = 0;
-            while(Cadena != null){
-                if(Caden[i] == ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'A' | 'B' | 'C'| 'D' | 'E' | 'F')){
-                    if(LiteralesTxt[i][1] == null){
-                        LiteralesTxt[i][1] = Cadena;
-                        // Eliminar el "0X" al principio antes de analizar la cadena como un número hexadecimal
-                        if (Cadena.startsWith("0X") || Cadena.startsWith("0x")) {
-                            Cadena = Cadena.substring(2);
-                        }
-                        // Analizar la cadena como un número hexadecimal
-                        int valor = Integer.parseInt(Cadena, 16);
-                        String cad = String.valueOf(valor);
-                        LiteralesTxt[i][2] = cad;
-                        return LiteralesTxt[i][2];
-                    }
+            while (i < 10 && (Identificadores[i][1] != null)) {
+                if (Identificadores[i][1] == null) {
+                    Identificadores[i][1] = "ID" + i;
+                    Identificadores[i][2] = cadena;
+                    return Identificadores[i][1];
                 }
                 i++;
-            } 
+            }
+        } else {
+            System.out.println("datos demasiado grandes");
+        }
+    }
+        return null;
+    }
 
-       }
-       return Cadena = null;
+
+    public String verificarLiteralesTxt(String cadena) {
+    if (cadena.length() > 0 && cadena.charAt(0) == '"') {
+        if (cadena.length() >= 2 && cadena.charAt(cadena.length() - 1) == '"') {
+            int i = 0;
+            while (i < 10 && (LiteralesTxt[i][1] != null)) {
+                if (LiteralesTxt[i][1] == null) {
+                    LiteralesTxt[i][1] = "TX" + i;
+                    LiteralesTxt[i][2] = cadena;
+                    return LiteralesTxt[i][1];
+                }
+                i++;
+            }
+        } else {
+            // ¿Qué debería suceder si la cadena de texto no cierra con comillas?
+            System.out.println("Error: La cadena de texto no cierra con comillas");
+        }
+    }
+        return null;
+    }
+    public String verificarLiteralesNum(String cadena) {
+        if (cadena.startsWith("0X") || cadena.startsWith("0x")) {
+            int i = 0;
+            while (i < 10 && LiteralesNum[i][1] != null) {
+                if (LiteralesNum[i][1] == null) {
+                    LiteralesNum[i][1] = cadena;
+                    cadena = cadena.substring(2);
+                    int valor = Integer.parseInt(cadena, 16);
+                    String cad = String.valueOf(valor);
+                    LiteralesNum[i][2] = cad;
+                    return LiteralesNum[i][2];
+                }
+                i++;
+            }
+        }
+        return null;
     }
 }
