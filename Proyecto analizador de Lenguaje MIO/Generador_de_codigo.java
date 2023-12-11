62fc9781c1d32bfcec4f7137b8060a4cd23bb7fd
JesusChay <112056157+JesusChay@users.noreply.github.com>
2023-12-11 02:33:43 -0600

    Escritor

--- Proyecto analizador de Lenguaje MIO/Generador_de_codigo.java
+++ Proyecto analizador de Lenguaje MIO/Generador_de_codigo.java
@@ -1,3 +1,29 @@
-public class Generador_de_codigo {
-    
+package analizador.sintactico.semantico.mio;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Escritor {
+    public void escribir(String fileName, List<String> content, boolean compilacionExitosa) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String line : content) {
+                writer.write(line);
+                writer.newLine();
+            }
+
+            if (compilacionExitosa) {
+                writer.write("Compilación exitosa");
+            } else {
+                writer.write("Error de sintaxis");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void escribir(String fileName, List<String> content) {
+        escribir(fileName, content, true); // Asumes compilación exitosa por defecto
+    }
 }
