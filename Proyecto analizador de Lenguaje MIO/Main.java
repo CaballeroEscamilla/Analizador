6acf91f1e7f402d4303828664c62c440cc48a8b7
JesusChay <112056157+JesusChay@users.noreply.github.com>
2023-12-11 02:34:50 -0600

    Update Main.java

--- Proyecto analizador de Lenguaje MIO/Main.java
+++ Proyecto analizador de Lenguaje MIO/Main.java
@@ -3,19 +3,24 @@ package analizador.sintactico.semantico.mio;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         List<String> List = new ArrayList<>();
         List<String> ListLex = new ArrayList<>();
         Lector reader = new Lector();
         Analizador_Lexico AnalizarLexi = new Analizador_Lexico();
-    
-        
+
         List = reader.read();
         for (String elemento : List) {
             System.out.println(elemento);
         }
 
         ListLex = AnalizarLexi.AnalizarLex(List);
+
+        AnalizadorSintactico analizadorSintactico = new AnalizadorSintactico(ListLex);
+        boolean compilacionExitosa = analizadorSintactico.analizar();
+
+        Escritor escritor = new Escritor();
+        escritor.escribir("factorial.sint", ListLex, compilacionExitosa);
     }
 }
